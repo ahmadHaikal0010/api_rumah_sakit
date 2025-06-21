09733c8b01263144011163341ce12978e4cdaead
Ahmad Haikal <haikala0010@gmail.com>
2025-06-21 08:58:17 +0700

    create migration

---
+++ database/migrations/2025_06_21_015337_create_rumah_sakits_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rumah_sakits', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama', 100);
+            $table->text('alamat');
+            $table->string('telpon', 20);
+            $table->double('latitude');
+            $table->double('longitude');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rumah_sakits');
+    }
+};
